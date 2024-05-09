2a564877fb2f9511f2e8b18087982fa43fb1be32
PC <PC@DESKTOP-165AL12>
2024-05-09 16:59:38 -0300

    subida del index.html que contiene el cuerpo principal del tp3

---
+++ src/main/java/ar/edu/unju/fi/Tp03RodriguezAlanApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp03RodriguezAlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp03RodriguezAlanApplication.class, args);
+	}
+
+}
